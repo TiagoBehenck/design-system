71b35a2200c7a81045c4118e382b3c954714abd6
Tiago Behenck <tiago.behenck.santos@gmail.com>
2022-10-22 18:40:35 -0300

    create the button component

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { ButtonProps, Button } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Create new',
+  },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'cancel',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Some icon
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+  },
+}
