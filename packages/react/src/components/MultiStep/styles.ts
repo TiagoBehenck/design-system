4ae7c9d21a41331041b1350934985c036eb05176
Tiago Behenck <tiago.behenck.santos@gmail.com>
2022-10-24 21:04:23 -0300

    create multi step component

---
+++ packages/react/src/components/MultiStep/styles.ts
@@ -0,0 +1,33 @@
+import { Text } from '../Text'
+import { styled } from '../../styles'
+
+export const MultiStepContainer = styled('div', {})
+
+export const Label = styled(Text, {
+  color: '$gray200',
+
+  defaultVariants: {
+    size: 'xs',
+  },
+})
+
+export const Steps = styled('div', {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(var(--steps-size), 1fr)',
+  gap: '$2',
+  marginTop: '$1',
+})
+
+export const Step = styled('div', {
+  height: '$1',
+  borderRadius: '$px',
+  backgroundColor: '$gray600',
+
+  variants: {
+    active: {
+      true: {
+        backgroundColor: '$gray100',
+      },
+    },
+  },
+})
