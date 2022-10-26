9865742b4161e422893f5fa4e052069fd8ac08d8
Tiago Behenck <tiago.behenck.santos@gmail.com>
2022-10-26 11:49:02 -0300

    fix the name of the path

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
+      config.base = '/design-system/'
     }
 
     return config
